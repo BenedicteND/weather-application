2bcfce0db97853bf1aee64dff3d925a59eb6d0f5
BenedicteND <Benedicte2000@live.dk>
2022-12-30 17:45:49 +0100

    Got API for forecast

--- src/index.js
+++ src/index.js
@@ -39,6 +39,12 @@ function formatDate(timestamp) {
   return `Last updated: ${day}, ${hours}:${minutes}`;
 }
 
+function getForecast(coordinates) {
+  let apiKey = `f5e814a04eddfab1740f07bf0328eee2`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   let trueTemperature = Math.round(response.data.main.temp);
   let currentTemperature = document.querySelector("#current-temperature");
@@ -58,7 +64,7 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   celsiusTemperature = Math.round(response.data.main.temp);
-  displayForecast();
+  getForecast(response.data.coord);
 }
 
 function retrieveWeatherInfo(city) {
@@ -109,7 +115,8 @@ function changeCelsius(event) {
   clickFahrenheit.classList.remove("active");
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
@@ -149,5 +156,3 @@ currentLocationButton.addEventListener("click", getPosition);
 changeDate();
 
 retrieveWeatherInfo("Copenhagen");
-
-displayForecast();
